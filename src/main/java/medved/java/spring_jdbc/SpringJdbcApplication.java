51fb1d52a96d85feeaa277739f8e72d0905c0a0d
Denis Fedorov <ssboshen@yandex.ru>
2022-12-20 20:33:20 +0300

    Runs query for search any product, which order customer, customer is a key to search

---
+++ src/main/java/medved/java/spring_jdbc/SpringJdbcApplication.java
@@ -0,0 +1,16 @@
+package medved.java.spring_jdbc;
+
+import org.springframework.boot.Banner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringJdbcApplication {
+
+    public static void main(String[] args) {
+        SpringApplication app = new SpringApplication(SpringJdbcApplication.class);
+        app.setBannerMode(Banner.Mode.OFF);
+        app.run(args);
+    }
+
+}
---
+++ src/main/java/medved/java/spring_jdbc/controller/CustomerController.java
@@ -0,0 +1,24 @@
+package medved.java.spring_jdbc.controller;
+
+import medved.java.spring_jdbc.repository.CustomerRepository;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/products")
+public class CustomerController {
+    private final CustomerRepository customerRepository;
+
+    public CustomerController(CustomerRepository customerRepository) {
+        this.customerRepository = customerRepository;
+    }
+
+    @GetMapping("/fetch-product")
+    private List<String> getProductName(@RequestParam(value = "name") String clientName){
+        return customerRepository.getProductName(clientName);
+    }
+}
